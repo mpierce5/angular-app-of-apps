c0f6fcf468df4e59b9fe950c6f7f026bf2d159ba
Michael Pierce <mpierce5@gmail.com>
2019-03-05 15:39:51 -0700

    initial commit angular-app-of-apps

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {App1SharedModule} from "../../projects/app1/src/app/app.module";
+import {App2SharedModule} from "../../projects/app2/src/app/app.module";
+
+const routes: Routes = [
+  {path: 'app1',
+    loadChildren: '../../projects/app1/src/app/app.module#App1SharedModule'},
+  {path: 'app2',
+    loadChildren: '../../projects/app2/src/app/app.module#App2SharedModule'},
+  { path: '**', redirectTo: '/app1/one'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),
+    App1SharedModule.forRoot(),
+    App2SharedModule.forRoot()
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { App1SharedModule } from "../../projects/app1/src/app/app.module";
+import { App2SharedModule } from "../../projects/app2/src/app/app.module";
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    App1SharedModule.forRoot(),
+    App2SharedModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
